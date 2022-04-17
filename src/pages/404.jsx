5442865206b4f74fb4f968694d49c9beb3c43c4c
Luis Marsiglia <luismarsiglia19@gmail.com>
2022-04-17 19:43:54 -0400

    improved 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,17 +1,29 @@
 import Head from 'next/head'
-import Layout, { siteTitle } from '@layouts/main'
-import { Heading, Container, Center } from '@chakra-ui/react'
+import Layout from '@layouts/main'
+import { Heading, Container, Center, HStack, StackDivider } from '@chakra-ui/react'
 
 export default function Custom404() {
 	return (
 		<Layout>
 			<Head>
-				<title>Page not found - {siteTitle}</title>
+				<title>404: This page could not be found</title>
 			</Head>
 
 			<Container>
 				<Center>
-					<Heading as='h1'>404 - Page not found</Heading>
+					<HStack
+						divider={<StackDivider minH='50px' borderColor='gray.400' />}
+						spacing={4}
+						align='center'
+					>
+						<Heading as='h1' fontWeight={600} fontSize={24}>
+							404
+						</Heading>
+
+						<Heading as='h2' fontWeight={400} fontSize={14}>
+							This page could not be found.
+						</Heading>
+					</HStack>
 				</Center>
 			</Container>
 		</Layout>
