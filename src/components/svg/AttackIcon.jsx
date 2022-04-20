67961c8263ecff6634cf73c48fb35e26fbaad6c7
Luis Marsiglia <luismarsiglia19@gmail.com>
2022-04-20 13:19:22 -0400

    ident eslint fixed

--- src/components/svg/AttackIcon.jsx
+++ src/components/svg/AttackIcon.jsx
@@ -1,14 +1,14 @@
 import { memo } from 'react'
 const icon = () => (
-	<svg
-		viewBox='0 0 8 15'
-		fill='#BB374F'
-		className='card_item__statistic_icon'
-	>
-		<g fillRule='evenodd'>
-			<path d='M3.964.5l-2.49 1.786.971 7.922H5.483l.97-7.922zM3.964 12.694H2.72v2.26h2.49v-2.26zM3.964 10.718H.667v1.452H7.262v-1.452z'></path>
-		</g>
-	</svg>
+  <svg
+    viewBox='0 0 8 15'
+    fill='#BB374F'
+    className='card_item__statistic_icon'
+  >
+    <g fillRule='evenodd'>
+      <path d='M3.964.5l-2.49 1.786.971 7.922H5.483l.97-7.922zM3.964 12.694H2.72v2.26h2.49v-2.26zM3.964 10.718H.667v1.452H7.262v-1.452z'></path>
+    </g>
+  </svg>
 )
 
 export default memo(icon)
