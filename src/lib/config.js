faa1a458fa235224e0fb4819f58af870b1e3c0e8
Luis Marsiglia <luismarsiglia19@gmail.com>
2022-04-21 21:13:56 -0400

    updated og:image url

--- src/lib/config.js
+++ src/lib/config.js
@@ -6,9 +6,9 @@ export const siteDescription = 'AxieHub is a fan site where you can find the bes
 
 export const siteKeywords = 'axie hub, axiehub, axie.hub, axiehub.vercel, axiehub vercel, axie infinity, axie tool, axie tools, play to earn, play2earn, p2e, crypto gaming, sky mavis, ronin, origin, axie infinity origin, origin cards, cards explorer, axie infinity cards, axie infinity cards explorer, origin education challenge'
 
-export const ogImageUrl = 'https://res.cloudinary.com/marsiglia/image/upload/v1650588866/axie-infinity/axie-hub-assets/axiehub_og_default_nilwop.png'
+export const ogImageUrl = 'https://res.cloudinary.com/marsiglia/image/upload/v1650589910/axie-infinity/axie-hub-assets/axiehub_og_default-lite_v6tj0d.png'
 
-export const ogTwitterImageUrl = 'https://res.cloudinary.com/marsiglia/image/upload/v1650588902/axie-infinity/axie-hub-assets/axiehub_og_twitter_r5h7fk.png'
+export const ogTwitterImageUrl = 'https://res.cloudinary.com/marsiglia/image/upload/v1650590015/axie-infinity/axie-hub-assets/axiehub_og_twitter-lite_hwr9s6.png'
 
 export const server =
   process.env.NODE_ENV !== 'production'
