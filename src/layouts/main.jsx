3d09f6725f49e82bb57393709bbe68c8935defc9
Luis Marsiglia <luismarsiglia19@gmail.com>
2022-04-23 00:16:31 -0400

    seo setup updated

--- src/layouts/main.jsx
+++ src/layouts/main.jsx
@@ -2,33 +2,32 @@ import Head from 'next/head'
 import { Flex } from '@chakra-ui/react'
 import Navbar from '@components/Navbar/index'
 import Footer from '@components/Footer'
-import {
-  siteTitle,
-  siteUrl,
-  siteDescription,
-  siteKeywords,
-  ogImageUrl,
-  ogTwitterImageUrl
-} from '@lib/config'
+import { siteConfig } from '@lib/seo'
 
 const HeadComponent = () => {
   return (
     <Head>
       <link rel='icon' href='/favicon.ico' />
-      <title>{siteTitle}</title>
-      <meta name='description' content={siteDescription} />
-      <meta name='keywords' content={siteKeywords} />
-      <meta property='og:type' content='website' />
-      <meta property='og:image' content={ogImageUrl} />
-      <meta name='og:title' content={siteTitle} />
-      <meta name='og:description' content={siteDescription} />
-      <meta property='og:url' content={siteUrl} />
-      <meta property='og:site_name' content={siteTitle} />
-      <meta name='twitter:card' content='summary_large_image' />
-      <meta name='twitter:title' content={siteTitle} />
-      <meta name='twitter:description' content={siteDescription} />
-      <meta name='twitter:creator' content='@marsigliaCR' />
-      <meta name='twitter:image' content={ogTwitterImageUrl} />
+      <title>{siteConfig.title}</title>
+      <meta name='description' content={siteConfig.description} />
+      <meta name='keywords' content={siteConfig.keywords} />
+      <meta property='og:type' content={siteConfig.type} />
+      <meta name='og:title' content={siteConfig.title} />
+      <meta property='og:site_name' content={siteConfig.title} />
+      <meta name='og:description' content={siteConfig.description} />
+      <meta property='og:url' content={siteConfig.url} />
+      <meta property='og:locale' content={siteConfig.locale} />
+      <meta property='og:image' content={siteConfig.ogImage} />
+      <meta property='og:image:width' content='2000' />
+      <meta property='og:image:height' content='1000' />
+      <meta name='twitter:card' content={siteConfig.twitterCard} />
+      <meta name='twitter:title' content={siteConfig.title} />
+      <meta name='twitter:description' content={siteConfig.description} />
+      <meta property='twitter:url' content={siteConfig.url} />
+      <meta name='twitter:image' content={siteConfig.twitterImage} />
+      <meta name='twitter:site' content={siteConfig.twitterSite} />
+      <meta name='twitter:creator' content={siteConfig.twitterCreator} />
+      <meta name='twitter:image:alt' content={siteConfig.twitterImageAlt} />
     </Head>
   )
 }
--- src/lib/config.js
+++ src/lib/config.js
@@ -1,17 +1,3 @@
-export const siteTitle = 'AxieHub'
-
-export const siteUrl = 'https://axiehub.vercel.app'
-
-export const siteDescription = 'AxieHub is a fan site where you can find the best tools related to Axie Infinity. Originally created for Origin Education Challenge!'
-
-export const siteKeywords = 'axie hub, axiehub, axie.hub, axiehub.vercel, axiehub vercel, axie infinity, axie tool, axie tools, play to earn, play2earn, p2e, crypto gaming, sky mavis, ronin, origin, axie infinity origin, origin cards, cards explorer, axie infinity cards, axie infinity cards explorer, origin education challenge'
-
-export const ogImageUrl =
-  'https://res.cloudinary.com/marsiglia/image/upload/v1650590910/axie-infinity/axie-hub-assets/axiehub_og_default-lite_vytzbf.png'
-
-export const ogTwitterImageUrl =
-  'https://res.cloudinary.com/marsiglia/image/upload/v1650590832/axie-infinity/axie-hub-assets/axiehub_og_twitter-lite_rynmyn.png'
-
 export const server =
   process.env.NODE_ENV !== 'production'
     ? 'http://localhost:5900'
