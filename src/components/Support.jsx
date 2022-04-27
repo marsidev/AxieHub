130641193f753f16da3027f053709e177a8f7a9f
Luis Marsiglia <luismarsiglia19@gmail.com>
2022-04-27 12:25:08 -0400

    improved styling (hover, bg) of support method inputs

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -8,38 +8,79 @@ import {
   FormControl,
   FormLabel,
   useClipboard,
+  useColorModeValue,
   VStack
 } from '@chakra-ui/react'
 import Modal from '@components/Modal'
+import { supportMethods } from '@lib/constants'
 import { FiCopy, FiCheck, FiExternalLink } from 'react-icons/fi'
-import { supportMethods } from '@/lib/constants'
 
 const Label = ({ id, label, value, isLink = false }) => {
   const { hasCopied, onCopy } = useClipboard(value, 3000)
 
-  const LabelIcon = isLink
-    ? FiExternalLink
-    : hasCopied
-      ? FiCheck : FiCopy
+  const LabelIcon = () => {
+    if (isLink) return <FiExternalLink />
+    if (hasCopied) return <FiCheck />
+    return <FiCopy />
+  }
+
+  const handleClick = () => {
+    if (isLink) return window.open(value, '_blank')
+    onCopy()
+  }
+
+  const colors = {
+    default: useColorModeValue('gray.100', 'gray.600'),
+    onHover: useColorModeValue('gray.200', 'gray.500'),
+    onCopied: useColorModeValue('green.200', 'green.500')
+  }
 
   return (
     <FormControl>
       <FormLabel htmlFor={id}>{label}</FormLabel>
-      <InputGroup size='sm'>
+      <InputGroup
+        size='sm'
+        bg={colors.default}
+        _hover={{ bg: colors.onHover }}
+        _focus={{ bg: colors.onHover }}
+        _active={{ bg: colors.onHover }}
+        borderRadius={8}
+      >
         <Input
           isReadOnly
           id={id}
           type='text'
           variant='filled'
           placeholder={value}
-          borderRadius={8}
+          borderRightRadius={0}
+          borderLeftRadius={8}
+          bg='inherit'
+          _focus={{ bg: 'inherit' }}
+          _hover={{ bg: 'inherit' }}
+          _active={{ bg: 'inherit' }}
+          transition='background 0.15s ease'
         />
-        <InputRightAddon p={0} borderRadius={8}>
+        <InputRightAddon
+          p={0}
+          borderRightRadius={8}
+          borderLeftRadius={0}
+          bg={hasCopied ? colors.onCopied : 'inherit'}
+          _focus={hasCopied ? colors.onCopied : 'inherit'}
+          _hover={hasCopied ? colors.onCopied : 'inherit'}
+          _active={hasCopied ? colors.onCopied : 'inherit'}
+        >
           <IconButton
             variant='ghost'
             size='sm'
             icon={<LabelIcon />}
-            onClick={!isLink ? onCopy : () => window.open(value)}
+            onClick={handleClick}
+            borderRightRadius={8}
+            borderLeftRadius={0}
+            bg='inherit'
+            _focus={{ bg: 'inherit' }}
+            _hover={{ bg: 'inherit' }}
+            _active={{ bg: 'inherit' }}
+            transition='background 0.15s ease'
           />
         </InputRightAddon>
       </InputGroup>
