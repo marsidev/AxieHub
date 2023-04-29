09e075136b987be2d01f16c5128e632c95ef4a2a
Luis Marsiglia <marsiglia.business@gmail.com>
2023-04-29 19:38:25 -0400

    chore: lint code and update node version

--- src/lib/seo.js
+++ src/lib/seo.js
@@ -1,20 +1,20 @@
-const title = "AxieHub";
+const title = 'AxieHub'
 
 export const siteConfig = {
   title,
   description:
-    "AxieHub is a fan site where you can find the best tools related to Axie Infinity. Originally created for Origin Education Challenge!",
+    'AxieHub is a fan site where you can find the best tools related to Axie Infinity. Originally created for Origin Education Challenge!',
   keywords:
-    "axie hub, axiehub, axie.hub, axiehub.vercel, axiehub vercel, axie infinity, axie tool, axie tools, play to earn, play2earn, p2e, crypto gaming, sky mavis, ronin, origin, axie infinity origin, origin cards, cards explorer, axie infinity cards, axie infinity cards explorer, origin education challenge",
-  url: "https://axiehub.vercel.app",
-  type: "website",
-  locale: "en_US",
+    'axie hub, axiehub, axie.hub, axiehub.vercel, axiehub vercel, axie infinity, axie tool, axie tools, play to earn, play2earn, p2e, crypto gaming, sky mavis, ronin, origin, axie infinity origin, origin cards, cards explorer, axie infinity cards, axie infinity cards explorer, origin education challenge',
+  url: 'https://axiehub.vercel.app',
+  type: 'website',
+  locale: 'en_US',
   ogImage:
-    "https://res.cloudinary.com/marsiglia/image/upload/v1650590910/axie-infinity/axie-hub-assets/axiehub_og_default-lite_vytzbf.png",
-  twitterCard: "summary_large_image",
+    'https://res.cloudinary.com/marsiglia/image/upload/v1650590910/axie-infinity/axie-hub-assets/axiehub_og_default-lite_vytzbf.png',
+  twitterCard: 'summary_large_image',
   twitterImage:
-    "https://res.cloudinary.com/marsiglia/image/upload/v1650590832/axie-infinity/axie-hub-assets/axiehub_og_twitter-lite_rynmyn.png",
-  twitterImageAlt: "Axie Hub",
-  twitterSite: "@marsidev",
-  twitterCreator: "@marsidev",
-};
+    'https://res.cloudinary.com/marsiglia/image/upload/v1650590832/axie-infinity/axie-hub-assets/axiehub_og_twitter-lite_rynmyn.png',
+  twitterImageAlt: 'Axie Hub',
+  twitterSite: '@marsidev',
+  twitterCreator: '@marsidev'
+}
